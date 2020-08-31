f17541c2b5cbd1189c845fad12135bcab778a2f7
jeesusmies <46649783+jeesusmies@users.noreply.github.com>
2020-08-31 16:29:54 +0300

    Add files via upload
    Added daily command and seperated embeds to a single method.

--- prog.py
+++ prog.py
@@ -6,6 +6,11 @@ import datetime
 import pickledb
 from owrap import Owrap
 from pymongo import MongoClient
+import asyncio
+
+##########################################################
+
+list_of_players_in_daily = []
 
 osu_token = "token"
 TOKEN = "token"
@@ -20,12 +25,21 @@ client.remove_command("help")
 db = mongoclient['database']
 collection = db['collection']
 
-def track_thing(r, uid, latest: bool = None):
+daily_db = mongoclient['daily_database']
+daily_collection = db['collection']
+
+##########################################################
+
+def track_thing(r, uid, latest: bool = None, daily: bool = None):
     now = datetime.datetime.now()
     osu = owrap.osu(r)
+    if daily == True:
+        c = daily_collection
+    else:
+        c = collection
     document = False
     try:
-        document = collection.find_one({'_id': uid})
+        document = c.find_one({'_id': uid})
     except:
         print("nothing found adding")
     finally:
@@ -36,31 +50,15 @@ def track_thing(r, uid, latest: bool = None):
             a.update({key: val})
         if document:
             if latest == True:
-                return collection.find_one({'_id': uid})
-            collection.update_one({'_id': uid}, {"$set": a})
+                return c.find_one({'_id': uid})
+            c.update_one({'_id': uid}, {"$set": a})
         else:
-            collection.insert_one(a)
+            c.insert_one(a)
                 
-    return collection.find_one({'_id': uid})
-@client.event
-async def on_ready():
-    await client.change_presence(activity=discord.Game("jimi help"))
-    print("work")
-# .format(sign2="+" if r['accuracy'] >= ll['accuracy'] else "-"
+    return c.find_one({'_id': uid})
 
-@commands.cooldown(1, 7, commands.BucketType.user)
-@client.command(pass_context=True, no_pm=True)
-async def profil(ctx, user: str = None):
-    if user == None:
-        user = setdb.get(str(ctx.message.author.id)) if setdb.get(str(ctx.message.author.id)) != False else ctx.message.author.name
-	
-    p = {"k": osu_token, "u": user}
-    r = requests.get("https://osu.ppy.sh/api/get_user", params=p).json()[0]
+def embed_maker(ll, r):
     a300, a100, a50 = client.get_emoji(748991269526700214), client.get_emoji(748991283758104698), client.get_emoji(748991293308272750)
-
-    ll = track_thing(r, int(str(round(ctx.message.author.id/10000000))+r['user_id']), True)
-    track_thing(r, int(str(round(ctx.message.author.id/10000000))+r['user_id']))
-
     sign1, sign2 ="-" if int(r['pp_rank']) >= ll['pp_rank'] else "+", "+" if float(r['accuracy']) >= ll['accuracy'] else ""
     
     embed=discord.Embed(color=0xff8f8f)
@@ -82,12 +80,57 @@ async def profil(ctx, user: str = None):
 **{a50}**: {format(int(r['count50']), ',d')} (+{format(int(r['count50'])-ll['count50'], ',d')})""", inline=True)
     
     embed.set_footer(text=f"Joined in {r['join_date']} || Playtime: {int(int(r['total_seconds_played'])/3600)}h || Last call: {ll['time']}")
-    await ctx.send(embed=embed)
-    #await ctx.send(f"{r['user_id']}\n{r['username']}\n{r['join_date']}\n{r['count300']}\n{r['count100']}\n{r['count50']}\n{r['playcount']}\n{r['ranked_score']}\n{r['total_score']}\n{r['pp_rank']}\n{r['level']}\n{r['pp_raw']}")
+
+    return embed
+    
+async def daily_refresh(uid):
+    list_of_players_in_daily.append(uid)
+    for x in range(0, 2):
+        p = {"k": osu_token, "u": uid}
+        r = requests.get("https://osu.ppy.sh/api/get_user", params=p).json()[0]
+        track_thing(r, r['user_id'], daily=True)
+        await asyncio.sleep(60*60*24)
+    list_of_players_in_daily.pop(uid)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game("jimi help"))
+    print("work")
+# .format(sign2="+" if r['accuracy'] >= ll['accuracy'] else "-"
+
+@commands.cooldown(1, 10, commands.BucketType.user)
+@client.command(pass_context=True, no_pm=True)
+async def profil(ctx, user: str = None):
+    if user == None:
+        user = setdb.get(str(ctx.message.author.id)) if setdb.get(str(ctx.message.author.id)) != False else ctx.message.author.name
+	
+    p = {"k": osu_token, "u": user}
+    r = requests.get("https://osu.ppy.sh/api/get_user", params=p).json()[0]
+    ll = track_thing(r, int(str(round(ctx.message.author.id/10000000))+r['user_id']), True)
+    track_thing(r, int(str(round(ctx.message.author.id/10000000))+r['user_id']))
+
+    await ctx.send(embed=embed_maker(ll, r))
+    #await ctx.send(f"{r['user_id']}\n{r['username']}\n{r['joinc['pp_rank']}\n{r['level']}\n{r['pp_raw']}")
+
+@commands.cooldown(1, 10, commands.BucketType.user)
+@client.command(pass_context=True, no_pm=True)
+async def daily(ctx, user: str = None):
+    if user == None:
+        user = setdb.get(str(ctx.message.author.id)) if setdb.get(str(ctx.message.author.id)) != False else ctx.message.author.name
+        
+    p = {"k": osu_token, "u": user}
+    r = requests.get("https://osu.ppy.sh/api/get_user", params=p).json()[0]
+    ll = track_thing(r, r['user_id'], True)
+
+    if r['user_id'] in list_of_players_in_daily:
+        await ctx.send(embed=embed_maker(ll, r))
+    else:
+        await ctx.send(f"added {user} to daily tracking. this will expire in 72 hours.")
+        await daily_refresh(r['user_id'])
 
 @client.command(pass_context=True)
 async def help(ctx):
-    await ctx.send("`jimi profil {name}` -> best profile shower with best tracking !\n`jimi daily {}` -> shows how much earned in a day (IN PROGRESS)\n`jimi set {name}` -> links assigned name with your discord id so that you can do `jimi profil` without specifing name")
+    await ctx.send("`jimi profil {name}` -> best profile shower with best tracking !\n`jimi daily {name}` -> shows how much earned in a day\n`jimi set {name}` -> links assigned name with your discord id so that you can do `jimi profil` without specifing name")
 
 @client.command(pass_context=True, no_pm=True)
 async def set(ctx, uname: str = None):
@@ -99,6 +142,22 @@ async def set(ctx, uname: str = None):
 
     setdb.set(str(ctx.message.author.id), uname)
     await ctx.send(f"added name {uname} to database")
+
+@profil.error
+async def profil_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
+        msg = 'this command is ratelimited, please try again in {:.2f}s'.format(error.retry_after)
+        await ctx.send(msg)
+    else:
+        raise error
+
+@daily.error
+async def daily_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
+        msg = 'this command is ratelimited, please try again in {:.2f}s'.format(error.retry_after)
+        await ctx.send(msg)
+    else:
+        raise error
     
 
 client.run(TOKEN)
